1e6b117ce9d2eaa2a0918c7da09dd36e21bf7726
Juuso <juuso.melentjeff@gmail.com>
2023-11-03 14:49:18 +0200

    feat(header): add initial Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./ui/Header";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center text-yellow-500">
-      Hello world!
-    </h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
