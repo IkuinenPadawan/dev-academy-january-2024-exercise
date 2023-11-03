085cfbed71332d9c58c3404f04be42f6eb1cd422
Juuso <juuso.melentjeff@gmail.com>
2023-11-03 15:00:02 +0200

    feat(search): add initial submit function

--- frontend/src/features/SearchStation.jsx
+++ frontend/src/features/SearchStation.jsx
@@ -3,8 +3,14 @@ import { useState } from "react";
 function SearchStation() {
   const [query, setQuery] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    setQuery("");
+  }
+
   return (
-    <form className="group relative">
+    <form className="group relative" onSubmit={handleSubmit}>
       <input
         placeholder="Search station..."
         value={query}
