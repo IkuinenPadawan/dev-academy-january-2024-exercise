223179b91a4337a160dec9a1fe881ca081b538d4
Juuso <juuso.melentjeff@gmail.com>
2023-11-03 15:57:49 +0200

    feat(search): add initial styling to search bar

--- frontend/src/features/SearchStation.jsx
+++ frontend/src/features/SearchStation.jsx
@@ -10,14 +10,17 @@ function SearchStation() {
   }
 
   return (
-    <form className="group relative" onSubmit={handleSubmit}>
+    <form className="group relative bg-slate-700" onSubmit={handleSubmit}>
       <input
         placeholder="Search station..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="focus:ring-2 focus:ring-amber-600 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 bg-slate-700 rounded-md py-2 pl-10 ring-1 ring-slate-900 shadow-sm"
+        type="text"
       />
     </form>
   );
 }
+//"focus:ring-2 focus:ring-blue-500 focus:outline-none rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"  aria-label="Filter projects" >
 
 export default SearchStation;
