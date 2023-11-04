99a7973c5ddb4bfdce943e8f99be4610daf31f06
Juuso <juuso.melentjeff@gmail.com>
2023-11-04 09:42:46 +0200

    feat(stationlist): add styling to StationItem

--- frontend/src/features/StationItem.jsx
+++ frontend/src/features/StationItem.jsx
@@ -1,9 +1,9 @@
 function StationItem() {
   return (
-    <li>
-      <div>
-        <p>Station name</p>
-        <p>City</p>
+    <li className="py-4 bg-slate-700 rounded shadow-md">
+      <div className="min-w-0 gap-x-4 ml-3">
+        <p className="text-sm font-medium text-slate-50">Station name</p>
+        <p className="text-sm text-slate-500 truncate">City</p>
       </div>
     </li>
   );
