5699bbf02208bc84bfda601d1a8a64111057da02
Juuso <juuso.melentjeff@gmail.com>
2023-11-06 13:59:37 +0200

    feat(db): change to using pool

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,10 +1,15 @@
-const { Client } = require('pg');
+const dotenv = require('dotenv');
+const { Pool } = require('pg');
 
-const client = new Client({
+dotenv.config({ path: './config.env' });
+
+const pool = new Pool({
   host: process.env.DB_LOCAL,
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
-client.connect().then(() => console.log('DB connection succesful'));
