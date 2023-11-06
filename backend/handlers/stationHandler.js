685c45f30e4ac9421c7b12dd567aaa0ca44f44d6
Juuso <juuso.melentjeff@gmail.com>
2023-11-06 09:20:59 +0200

    feat(handler): add initial get station func

--- backend/handlers/stationHandler.js
+++ backend/handlers/stationHandler.js
@@ -5,3 +5,11 @@ exports.getAllStations = (req, res, next) => {
   });
   next();
 };
+
+exports.getStation = (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Not yet implemented',
+  });
+  next();
+};
