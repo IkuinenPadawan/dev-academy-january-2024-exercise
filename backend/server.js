365d8f7fcd54dad061095613350835923c507080
Juuso <juuso.melentjeff@gmail.com>
2023-11-06 10:39:19 +0200

    feat(db): connect to db on server start

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const dotenv = require('dotenv');
+const { Client } = require('pg');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const client = new Client({
+  host: process.env.DB_LOCAL,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+client.connect().then(() => console.log('DB connection succesful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
