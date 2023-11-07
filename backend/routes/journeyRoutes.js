3ddf6a4a2f69b5dff2dc077aee2a8757cc52497d
Juuso <juuso.melentjeff@gmail.com>
2023-11-07 19:57:23 +0200

    feat(routes): add journey route

--- backend/routes/journeyRoutes.js
+++ backend/routes/journeyRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const journeyHandler = require('./../handlers/journeyHandler');
 
 const router = express.Router();
+
+router.route('/').get(journeyHandler.getJourneyStats);
 
 module.exports = router;
