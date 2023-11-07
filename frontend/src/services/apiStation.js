b5ad99d409e40c86110e8eb985cfb62d641b24cb
Juuso <juuso.melentjeff@gmail.com>
2023-11-07 13:48:23 +0200

    feat(fetch): add get all stations func

--- frontend/src/services/apiStation.js
+++ frontend/src/services/apiStation.js
@@ -1,1 +1,11 @@
 const API_URL = "http://localhost:3000/api";
+
+export async function getStations() {
+  const res = await fetch(`${API_URL}/stations`);
+
+  if (!res.ok) throw Error("Failed getting stations");
+
+  const { data } = await res.json();
+  console.log(data);
+  return data;
+}
