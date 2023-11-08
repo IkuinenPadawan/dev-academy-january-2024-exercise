eceab0ce2385087bcaff1c515a97932cf310a439
Juuso <juuso.melentjeff@gmail.com>
2023-11-08 10:55:17 +0200

    feat(api): add get journey stats func

--- frontend/src/services/apiStation.js
+++ frontend/src/services/apiStation.js
@@ -17,3 +17,11 @@ export async function getStation(id) {
   const { data } = await res.json();
   return data;
 }
+
+export async function getJourneyStats(id) {
+  const res = await fetch(`${API_URL}/journeys/stationstats/${id}`);
+  if (!res.ok) throw Error(`Couldn't find stats #${id}`);
+
+  const { data } = await res.json();
+  return data;
+}
