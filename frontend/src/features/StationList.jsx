a2e0c00fbe5ad0cdf0f6949092a9fe61614f14c4
Juuso <juuso.melentjeff@gmail.com>
2023-11-08 14:08:53 +0200

    feat(stationlist): use loader data in component

--- frontend/src/features/StationList.jsx
+++ frontend/src/features/StationList.jsx
@@ -1,7 +1,9 @@
+import { useLoaderData } from "react-router-dom";
 import StationItem from "./StationItem";
 import { getStations } from "../services/apiStation";
 
 function StationList() {
+  const stations = useLoaderData();
   return (
     <ul role="list" className="m-3 flex flex-col gap-2">
       <StationItem />
