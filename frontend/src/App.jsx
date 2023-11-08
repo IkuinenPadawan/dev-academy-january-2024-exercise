55c33fc9d54b60ee8bab9201b6c6f0dd53412ae2
Juuso <juuso.melentjeff@gmail.com>
2023-11-08 14:07:37 +0200

    feat(stationlist): add stationlist loader to router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import StationList from "./features/StationList";
 import StationView from "./features/StationView";
 import { loader as stationLoader } from "./features/StationView";
+import { loader as stationsLoader } from "./features/StationList";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <StationList />,
+    loader: stationsLoader,
   },
   {
     path: "/station/:stationId",
