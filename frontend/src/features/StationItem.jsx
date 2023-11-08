3a677b53f01420c770ff1befb31979afb188f597
Juuso <juuso.melentjeff@gmail.com>
2023-11-08 16:10:47 +0200

    feat(stationlist): render station data dynamically in list item

--- frontend/src/features/StationItem.jsx
+++ frontend/src/features/StationItem.jsx
@@ -1,9 +1,13 @@
-function StationItem() {
+function StationItem({ station }) {
   return (
     <li className="py-4 bg-slate-700 rounded shadow-md">
       <div className="min-w-0 gap-x-4 ml-3">
-        <p className="text-sm font-medium text-slate-50">Station name</p>
-        <p className="text-sm text-slate-500 truncate">City</p>
+        <p className="text-sm font-medium text-slate-50">
+          {station.station_name}
+        </p>
+        <p className="text-sm text-slate-500 truncate">
+          {station.station_address}
+        </p>
       </div>
     </li>
   );
