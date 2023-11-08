1f1e9521e5fb2f5d9877535e5aa0af30ac682efc
Juuso <juuso.melentjeff@gmail.com>
2023-11-08 09:32:48 +0200

    feat(handler): add initial get journey stats func

--- backend/handlers/journeyHandler.js
+++ backend/handlers/journeyHandler.js
@@ -1,3 +1,16 @@
 const db = require('../database/index');
 
-exports.getJourneyStats = async (req, res, next) => {};
+exports.getJourneyStats = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: 'success',
+      message: 'Not yet implemented',
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: { err },
+    });
+  }
+  next();
+};
