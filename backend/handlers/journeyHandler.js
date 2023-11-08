26aa8ce0ac4ed3a18963218c5fe9f0635eb7d6c4
Juuso <juuso.melentjeff@gmail.com>
2023-11-08 10:08:16 +0200

    feat(handler): add get stats queries

--- backend/handlers/journeyHandler.js
+++ backend/handlers/journeyHandler.js
@@ -1,10 +1,33 @@
 const db = require('../database/index');
 
 exports.getJourneyStats = async (req, res, next) => {
+  const numStartingQuery =
+    'SELECT COUNT(*) FROM Journey WHERE departure_station_id = $1';
+  const numEndingQuery =
+    'SELECT COUNT(*) FROM Journey WHERE return_station_id = $1';
+  const distStartingQuery =
+    'SELECT AVG(distance) FROM Journey WHERE departure_station_id = $1';
+  const durJStartingQuery =
+    'SELECT AVG(duration) FROM Journey WHERE departure_station_id = $1';
   try {
+    const numStarting = await db.query(numStartingQuery, [
+      req.params.stationId,
+    ]);
+    const numEnding = await db.query(numEndingQuery, [req.params.stationId]);
+    const distStarting = await db.query(distStartingQuery, [
+      req.params.stationId,
+    ]);
+    const durStarting = await db.query(durJStartingQuery, [
+      req.params.stationId,
+    ]);
     res.status(200).json({
       status: 'success',
-      message: 'Not yet implemented',
+      data: {
+        numberOfJourneysStarting: numStarting.rows[0].count,
+        numberOfJourneysEnding: numEnding.rows[0].count,
+        averageDistanceOfJourneysStarting: distStarting.rows[0].avg,
+        averageDurationOfJourneysStarting: durStarting.rows[0].avg,
+      },
     });
   } catch (err) {
     res.status(404).json({
