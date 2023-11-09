9b34b2b1f5b06b64c92520ae23e86254e25935ae
Juuso <juuso.melentjeff@gmail.com>
2023-11-09 21:20:09 +0200

    feat(pagination): add next/prev button functions

--- frontend/src/ui/Pagination.jsx
+++ frontend/src/ui/Pagination.jsx
@@ -5,16 +5,26 @@ function Pagination({ count, searchParams, setSearchParams }) {
 
   const pageCount = Math.ceil(count / 10);
 
+  function nextPage() {
+    const next = currentPage === pageCount ? currentPage : currentPage + 1;
+
+    searchParams.set("page", next);
+    setSearchParams(searchParams);
+  }
+  function prevPage() {
+    const prev = currentPage === 1 ? currentPage : currentPage - 1;
+
+    searchParams.set("page", prev);
+    setSearchParams(searchParams);
+  }
+
   return (
     <div className="flex flex-row justify-between text-slate-50">
       <p className="flex gap-1">
         Showing{" "}
-        <span className="font-bold font">
-          {(currentPage - 1) * PAGE_LIMIT + 1}
-        </span>{" "}
-        to{" "}
+        <span className="font-bold font">{(currentPage - 1) * 10 + 1}</span> to{" "}
         <span className="font-bold">
-          {currentPage === pageCount ? count : currentPage * PAGE_LIMIT}
+          {currentPage === pageCount ? count : currentPage * 10}
         </span>{" "}
         of <span className="font-bold">{count}</span> results
       </p>
