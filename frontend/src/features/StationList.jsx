b1e7394d997afdebd78fb921ec7e862d9f2c0b52
Juuso <juuso.melentjeff@gmail.com>
2023-11-09 18:43:07 +0200

    refactor(stationlist): remove loader related code

--- frontend/src/features/StationList.jsx
+++ frontend/src/features/StationList.jsx
@@ -1,10 +1,7 @@
-import { useLoaderData } from "react-router-dom";
 import StationItem from "./StationItem";
 import Pagination from "../ui/Pagination";
-import { getStations } from "../services/apiStation";
 
 function StationList() {
-  const stations = useLoaderData();
   return (
     <div>
       <ul role="list" className="m-3 flex flex-col gap-2">
@@ -15,11 +12,6 @@ function StationList() {
       <Pagination />
     </div>
   );
-}
-
-export async function loader() {
-  const stations = await getStations();
-  return stations;
 }
 
 export default StationList;
