87047acc70997eca2bf9f6803dd8ab20c069d487
Juuso <juuso.melentjeff@gmail.com>
2023-11-09 20:24:50 +0200

    refactor(stationlist): move data fetch to useEffect

--- frontend/src/features/StationList.jsx
+++ frontend/src/features/StationList.jsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from "react";
 import StationItem from "./StationItem";
 import Pagination from "../ui/Pagination";
+import { getStations } from "../services/apiStation";
 
 function StationList() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchStations();
+  }, []);
+
+  const fetchStations = async () => {
+    const stations = await getStations();
+    setData(stations);
+    setIsLoading(false);
+  };
+
   return (
     <div>
       <ul role="list" className="m-3 flex flex-col gap-2">
-        {stations.stations.map((station) => (
-          <StationItem station={station} key={station.id} />
-        ))}
+        {isLoading && <p>Loading...</p>}
+        {!isLoading &&
+          data.stations.map((station) => (
+            <StationItem station={station} key={station.id} />
+          ))}
       </ul>
       <Pagination />
     </div>
