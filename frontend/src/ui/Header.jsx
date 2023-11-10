15bf08fcbc8d8dfa9a5870ef71a1b2f7d36734c6
Juuso <juuso.melentjeff@gmail.com>
2023-11-10 10:07:05 +0200

    feat(header): add app name and flex

--- frontend/src/ui/Header.jsx
+++ frontend/src/ui/Header.jsx
@@ -2,7 +2,10 @@ import SearchStation from "../features/SearchStation";
 
 function Header() {
   return (
-    <header className="p-4">
+    <header className="flex items-center justify-between p-4 bg-slate-700">
+      <h1 className="uppercase tracking-widest text-bold text-slate-50">
+        Bike Station Info
+      </h1>
       <SearchStation />
     </header>
   );
