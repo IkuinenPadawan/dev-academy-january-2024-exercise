d9973bda3a05c5080a85ef69958a377948d4b496
Juuso <juuso.melentjeff@gmail.com>
2023-11-10 16:17:49 +0200

    feat(sort): add initial change order functionality

--- frontend/src/ui/Sort.jsx
+++ frontend/src/ui/Sort.jsx
@@ -1,10 +1,11 @@
-function Sort() {
+function Sort({ ascending, setAscending }) {
   return (
     <button
       id="dropdownDefaultButton"
       data-dropdown-toggle="dropdown"
       className="my-1 text-slate-50 bg-blue-500 hover:bg-blue-400 rounded-md font-medium text-sm px-5 py-2.5 text-center inline-flex items-center"
       type="button"
+      onClick={() => setAsceding(!ascending)}
     >
       Sort by station name
     </button>
