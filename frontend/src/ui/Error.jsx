7738cccb192e61577ce7ffebcf16cc559d4b2bf4
Juuso <juuso.melentjeff@gmail.com>
2023-11-10 10:27:59 +0200

    feat: add navigate back functionality to error

--- frontend/src/ui/Error.jsx
+++ frontend/src/ui/Error.jsx
@@ -1,12 +1,14 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, useNavigate } from "react-router-dom";
 
 function Error() {
+  const navigate = useNavigate();
   const error = useRouteError();
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
       <p>{error.data || error.message}</p>
+      <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
 }
