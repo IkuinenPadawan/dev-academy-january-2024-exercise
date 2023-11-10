271fd01c3a05dd7f5d0dddbc25c902eaa765d886
Juuso <juuso.melentjeff@gmail.com>
2023-11-10 16:09:21 +0200

    feat(sort): add initial Sort component

---
+++ frontend/src/ui/Sort.jsx
@@ -0,0 +1,14 @@
+function Sort() {
+  return (
+    <button
+      id="dropdownDefaultButton"
+      data-dropdown-toggle="dropdown"
+      className="my-1 text-slate-50 bg-blue-500 hover:bg-blue-400 rounded-md font-medium text-sm px-5 py-2.5 text-center inline-flex items-center"
+      type="button"
+    >
+      Sort by station name
+    </button>
+  );
+}
+
+export default Sort;
