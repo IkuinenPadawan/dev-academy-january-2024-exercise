df17fbf348650488f3da2ea483e880a168f31690
Juuso <juuso.melentjeff@gmail.com>
2023-11-10 10:26:12 +0200

    feat: add error element to router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import StationList from "./features/StationList";
 import StationView from "./features/StationView";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 // Loaders
 import { loader as stationLoader } from "./features/StationView";
@@ -12,15 +13,18 @@ import { loader as stationLoader } from "./features/StationView";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
         element: <StationList />,
+        errorElement: <Error />,
       },
       {
         path: "/station/:stationId",
         element: <StationView />,
         loader: stationLoader,
+        errorElement: <Error />,
       },
     ],
   },
