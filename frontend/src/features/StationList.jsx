38bc69b9a5673dc06a1e1c96795320d863e48f4b
Juuso <juuso.melentjeff@gmail.com>
2023-11-10 08:49:53 +0200

    feat(pagination): fetch data dynamically based on searchparams

--- frontend/src/features/StationList.jsx
+++ frontend/src/features/StationList.jsx
@@ -7,30 +7,35 @@ import { useSearchParams } from "react-router-dom";
 
 function StationList() {
   const [data, setData] = useState();
+  const [count, setCount] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
   useEffect(() => {
     fetchStations();
   }, [searchParams]);
 
   const fetchStations = async () => {
-    const stations = await getStations(searchParams.get("page"));
+    const stations = await getStations(page);
     setData(stations);
+    setCount(stations.stationCount[0].count);
     setIsLoading(false);
   };
 
   return (
     <div>
-      <ul role="list" className="m-3 flex flex-col gap-2">
-        {isLoading && <p>Loading...</p>}
-        {!isLoading &&
-          data.stations.map((station) => (
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && (
+        <ul role="list" className="m-3 flex flex-col gap-2">
+          {data.stations.map((station) => (
             <StationItem station={station} key={station.id} />
           ))}
-      </ul>
+        </ul>
+      )}
       <Pagination
-        count={100}
+        count={count}
         searchParams={searchParams}
         setSearchParams={setSearchParams}
       />
