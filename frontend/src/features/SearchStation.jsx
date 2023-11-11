4ba629e299c9be037ca171377f3df50c5523d91f
Juuso <juuso.melentjeff@gmail.com>
2023-11-11 10:05:00 +0200

    refactor: use Search Icon component

--- frontend/src/features/SearchStation.jsx
+++ frontend/src/features/SearchStation.jsx
@@ -1,6 +1,8 @@
 import { useRef, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
+import SearchIcon from "../ui/SearchIcon";
+
 function SearchStation() {
   const [searchParams, setSearchParams] = useSearchParams();
   const inputRef = useRef(null);
@@ -48,19 +50,8 @@ function SearchStation() {
 
   return (
     <div className="group relative bg-slate-700 m-3">
-      <svg
-        width="20"
-        height="20"
-        fill="currentColor"
-        className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-600"
-        aria-hidden="true"
-      >
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-        />
-      </svg>
+      <SearchIcon className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-600" />
+
       <input
         placeholder="Search station..."
         id="search"
