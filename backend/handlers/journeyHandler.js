dd22751301fa9ccb4686eb8d95370295c9502135
Juuso <juuso.melentjeff@gmail.com>
2023-11-12 00:15:51 +0200

    refactor(errors): wrap handler funcs with catchAsync

--- backend/handlers/journeyHandler.js
+++ backend/handlers/journeyHandler.js
@@ -1,6 +1,8 @@
 const db = require('../database/index');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.getJourneyStats = async (req, res, next) => {
+exports.getJourneyStats = catchAsync(async (req, res, next) => {
   const numStartingQuery =
     'SELECT COUNT(*) FROM Journey WHERE departure_station_id = $1';
   const numEndingQuery =
@@ -9,31 +11,34 @@ exports.getJourneyStats = async (req, res, next) => {
     'SELECT AVG(distance) FROM Journey WHERE departure_station_id = $1';
   const durJStartingQuery =
     'SELECT AVG(duration) FROM Journey WHERE departure_station_id = $1';
-  try {
-    const numStarting = await db.query(numStartingQuery, [
-      req.params.stationId,
-    ]);
-    const numEnding = await db.query(numEndingQuery, [req.params.stationId]);
-    const distStarting = await db.query(distStartingQuery, [
-      req.params.stationId,
-    ]);
-    const durStarting = await db.query(durJStartingQuery, [
-      req.params.stationId,
-    ]);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        numberOfJourneysStarting: numStarting.rows[0].count,
-        numberOfJourneysEnding: numEnding.rows[0].count,
-        averageDistanceOfJourneysStarting: distStarting.rows[0].avg,
-        averageDurationOfJourneysStarting: durStarting.rows[0].avg,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Statistics with the specified station ID were not found.',
-    });
+
+  const numStarting = await db.query(numStartingQuery, [req.params.stationId]);
+  const numEnding = await db.query(numEndingQuery, [req.params.stationId]);
+  const distStarting = await db.query(distStartingQuery, [
+    req.params.stationId,
+  ]);
+  const durStarting = await db.query(durJStartingQuery, [req.params.stationId]);
+
+  if (
+    numStarting.rows[0].count === '0' ||
+    numEnding.rows[0].count === '0' ||
+    distStarting.rows[0].avg ||
+    durStarting.rows[0].avg
+  ) {
+    return next(
+      new AppError(
+        'Statistics with the specified station ID were not found.',
+        404,
+      ),
+    );
   }
-  next();
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      numberOfJourneysStarting: numStarting.rows[0].count,
+      numberOfJourneysEnding: numEnding.rows[0].count,
+      averageDistanceOfJourneysStarting: distStarting.rows[0].avg,
+      averageDurationOfJourneysStarting: durStarting.rows[0].avg,
+    },
+  });
+});
--- backend/handlers/stationHandler.js
+++ backend/handlers/stationHandler.js
@@ -1,60 +1,53 @@
 const db = require('../database/index');
 const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.getAllStations = async (req, res, next) => {
-  try {
-    const { page, limit, search } = req.query;
-    const values = [page, limit];
-    const query = 'SELECT * FROM station';
-    let countQuery = 'SELECT count(*) as count FROM station';
+exports.getAllStations = catchAsync(async (req, res, next) => {
+  const { page, limit, search } = req.query;
+  const values = [page, limit];
+  const query = 'SELECT * FROM station';
+  let countQuery = 'SELECT count(*) as count FROM station';
 
-    if (search) {
-      values.push(`%${req.query.search}%`);
-      countQuery += ' WHERE station_name ILIKE $1';
-    }
-
-    const stationCountResult = await db.query(countQuery, values.slice(2));
-    const stationCount = stationCountResult.rows[0].count;
-
-    const features = new APIFeatures(query, req.query, 'station_name')
-      .search()
-      .sort()
-      .paginate();
-
-    const results = await db.query(features.query, values);
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        stations: results.rows,
-        stationCount,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'No stations were found',
-    });
+  if (search) {
+    values.push(`%${req.query.search}%`);
+    countQuery += ' WHERE station_name ILIKE $1';
   }
-  next();
-};
 
-exports.getStation = async (req, res, next) => {
-  try {
-    const results = await db.query('SELECT * FROM station WHERE id = $1', [
-      req.params.id,
-    ]);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        station: results.rows[0],
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'A station with the specified ID was not found.',
-    });
+  const stationCountResult = await db.query(countQuery, values.slice(2));
+  const stationCount = stationCountResult.rows[0].count;
+
+  const features = new APIFeatures(query, req.query, 'station_name')
+    .search()
+    .sort()
+    .paginate();
+
+  const results = await db.query(features.query, values);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stations: results.rows,
+      stationCount,
+    },
+  });
+});
+
+exports.getStation = catchAsync(async (req, res, next) => {
+  const results = await db.query('SELECT * FROM station WHERE id = $1', [
+    req.params.id,
+  ]);
+
+  if (results.rowCount === 0) {
+    return next(
+      new AppError('A station with the specified ID was not found.', 404),
+    );
   }
-  next();
-};
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      station: results.rows[0],
+    },
+  });
+});
