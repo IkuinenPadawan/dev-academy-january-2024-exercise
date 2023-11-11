68030bd1ef62a1ade0e9aed7d5a4279364b91457
Juuso <juuso.melentjeff@gmail.com>
2023-11-12 00:12:12 +0200

    feat(errors): add global error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -39,4 +39,15 @@ app.all('*', (req, res, next) => {
   });
 });
 
+// Global error handling
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
