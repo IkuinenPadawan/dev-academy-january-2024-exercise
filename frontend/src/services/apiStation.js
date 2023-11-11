df9966394fd8772324f1fe92825ccd94a033a5ff
Juuso <juuso.melentjeff@gmail.com>
2023-11-11 13:23:25 +0200

    docs: add comments to apiStation file

--- frontend/src/services/apiStation.js
+++ frontend/src/services/apiStation.js
@@ -1,5 +1,7 @@
+// API base URL
 const API_URL = "http://localhost:3000/api";
 
+// GET stations
 export async function getStations(page = 1, limit = 10, order = "asc", search) {
   let url = `${API_URL}/stations?page=${page}&limit=${limit}&sortBy=station_name&order=${order}`;
 
@@ -18,6 +20,7 @@ export async function getStations(page = 1, limit = 10, order = "asc", search) {
   return data;
 }
 
+// GET station by ID
 export async function getStation(id) {
   const res = await fetch(`${API_URL}/stations/${id}`);
   if (!res.ok) throw Error(`Couldn't find order #${id}`);
@@ -26,6 +29,7 @@ export async function getStation(id) {
   return data;
 }
 
+// GET Journey stats by station ID
 export async function getJourneyStats(id) {
   const res = await fetch(`${API_URL}/journeys/stationstats/${id}`);
   if (!res.ok) throw Error(`Couldn't find stats #${id}`);
