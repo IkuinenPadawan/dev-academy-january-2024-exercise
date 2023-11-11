97ef383b7cb498956d744214ce8489de9eed4e1e
Juuso <juuso.melentjeff@gmail.com>
2023-11-11 23:00:07 +0200

    docs: add comments to components

--- frontend/src/ui/Error.jsx
+++ frontend/src/ui/Error.jsx
@@ -1,3 +1,4 @@
+// Import modules
 import { useRouteError, useNavigate } from "react-router-dom";
 
 function Error() {
--- frontend/src/ui/icons/SearchIcon.jsx
+++ frontend/src/ui/icons/SearchIcon.jsx
@@ -1,3 +1,4 @@
+// Import modules
 import PropTypes from "prop-types";
 
 function SearchIcon({ className }) {
