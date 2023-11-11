11673359bbef4bbc658e7e31cfdb13a90b858cfc
Juuso <juuso.melentjeff@gmail.com>
2023-11-11 22:06:18 +0200

    feat(apidoc): add swagger options

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,23 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Bike Station Info API',
+      version: '1.0.0',
+      descritpion: 'A simple Express Helsinki City Bike Station Info API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
 const stationRouter = require('./routes/stationRoutes');
 const journeyRouter = require('./routes/journeyRoutes');
 
