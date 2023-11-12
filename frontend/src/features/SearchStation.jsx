9006a5f7ba3d834927c62a883ba057a7dd5ca52b
Juuso <juuso.melentjeff@gmail.com>
2023-11-12 16:09:22 +0200

    docs: remove leftover comments

--- frontend/src/features/SearchStation.jsx
+++ frontend/src/features/SearchStation.jsx
@@ -67,6 +67,5 @@ function SearchStation() {
     </div>
   );
 }
-//"focus:ring-2 focus:ring-blue-500 focus:outline-none rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"  aria-label="Filter projects" >
 
 export default SearchStation;
